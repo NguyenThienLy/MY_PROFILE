7fa560f0a31cdcd6e87a2c04179c76adc8205b07
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2020-04-12 12:10:14 +0700

    test route

--- docs/js/page/index.js
+++ docs/js/page/index.js
@@ -1,22 +1,24 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   if (document.documentElement.scrollTop > 1800 && !isShowedProjects) {
 
-    fetch('../../../data/projects.json')
+    fetch('../../../docs/data/projects.json')
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        console.log("data", data)
         isShowedProjects = true;
 
         projects = data;
         renderProjects();
+      }).catch((err) => {
+        console.log("err", err)
+
       });
   }
 
   if (document.documentElement.scrollTop > 3600 && !isShowedOtherProjects) {
 
-    fetch('../../../data/otherProjects.json')
+    fetch('../../../docs/data/otherProjects.json')
       .then((response) => {
         return response.json();
       })
@@ -33,7 +35,7 @@ document.addEventListener('scroll', (event) => {
   setTimeout(() => {
     if (document.documentElement.scrollTop > 1800 && !isShowedProjects) {
 
-      fetch('../../../data/projects.json')
+      fetch('../../../docs/data/projects.json')
         .then((response) => {
           return response.json();
         })
@@ -47,7 +49,7 @@ document.addEventListener('scroll', (event) => {
 
     if (document.documentElement.scrollTop > 3600 && !isShowedOtherProjects) {
 
-      fetch('../../../data/otherProjects.json')
+      fetch('../../../docs/data/otherProjects.json')
         .then((response) => {
           return response.json();
         })
